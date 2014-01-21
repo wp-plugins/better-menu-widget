b6570a4bcccc08db97bbff51ccdb6df82e58ec7b
TraceyHolinka <TraceyHolinka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-21 22:50:41 +0000

    Internationalize plugin.
    git-svn-id: https://plugins.svn.wordpress.org/better-menu-widget/tags/1.2@842724 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ better-menu-widget.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * Plugin Name:  Better Menu Widget
+ * Plugin URI:  http://www.chaostoclarity.com/wordpress-plugins/better-menu-widget/
+ * Description:  Better Menu Widget makes it easy to customize your menu widgets by adding css styles and a heading link.
+ * Version:      1.2
+ * Author:       Tracey Holinka
+ * Author URI:   http://TraceyHolinka.com
+ * Author Email: tracey@chaostoclarity.com
+ * License:      GPLv2
+ * 
+ *  Copyright 2010-2013 Tracey Holinka (tracey@chaostoclarity.com)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 2, as 
+ *  published by the Free Software Foundation.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  
+*/
+ 
+if (!class_exists("Better_Menu_Widget")) {
+
+add_action('widgets_init', 'load_better_menu_widget');
+
+	function load_better_menu_widget() {
+		register_widget( 'Better_Menu_Widget' );
+	}
+
+	class Better_Menu_Widget extends WP_Widget {
+
+		function __construct() {
+
+		load_plugin_textdomain( 'better-menu-widget', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		
+			$widget_ops = array( 'classname' => 'better-menu-widget', 'description' => __('Add one of your custom menus as a widget.', 'better-menu-widget') );
+			$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'better-menu-widget' );
+			$this->WP_Widget( 'better-menu-widget', __('Better Menu', 'better-menu-widget'), $widget_ops, $control_ops );	
+		}
+
+		function widget($args, $instance) {
+			
+			$nav_menu = wp_get_nav_menu_object( $instance['nav_menu'] ); /* Get menu */
+	
+			if ( !$nav_menu )
+				return;
+	
+			$instance['title'] = apply_filters('widget_title', $instance['title'], $instance, $this->id_base);
+	
+			echo $args['before_widget'];
+	
+			if ( !empty($instance['title']) && !empty($instance['title_url']) )
+				echo $args['before_title'] . '<a href="' . $instance['title_url'] . '">' . $instance['title'] . '</a>' . $args['after_title'];
+	
+			if ( !empty($instance['title']) && empty($instance['title_url']) )
+				echo $args['before_title'] . $instance['title'] . $args['after_title'];
+	
+			wp_nav_menu( array( 'fallback_cb' => '', 'menu' => $nav_menu, 'menu_class' => $instance['menu_class'], 'container' => false ) );
+	
+			echo $args['after_widget'];
+		}
+
+		// widget admin
+			
+		function update( $new_instance, $old_instance ) {
+			$instance['title'] = strip_tags( stripslashes($new_instance['title']) );
+			$instance['nav_menu'] = (int) $new_instance['nav_menu'];
+			$instance['title_url'] = $new_instance['title_url'];
+			$instance['menu_class'] = $new_instance['menu_class'];
+			return $instance;
+		}
+	
+		function form( $instance ) {
+			$title = isset( $instance['title'] ) ? $instance['title'] : '';
+			$nav_menu = isset( $instance['nav_menu'] ) ? $instance['nav_menu'] : '';
+			$title_url = isset( $instance['title_url'] ) ? $instance['title_url'] : '';
+			$menu_class = isset( $instance['menu_class'] ) ? $instance['menu_class'] : 'sub-menu';
+	
+			// Get menus
+			$menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+	
+			// If no menus exists, direct the user to create some.
+			if ( !$menus ) {
+				echo '<p>'. sprintf( __('No menus have been created yet. <a href="%s">Create some</a>.', 'better-menu-widget'), admin_url('nav-menus.php') ) .'</p>';
+				return;
+			}
+			?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'better-menu-widget') ?></label><input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" /></p>
+			<p><label for="<?php echo $this->get_field_id('title_url'); ?>"><?php _e('Title URL:', 'better-menu-widget') ?></label><input type="text" class="widefat" id="<?php echo $this->get_field_id('title_url'); ?>" name="<?php echo $this->get_field_name('title_url'); ?>" value="<?php echo $title_url; ?>" /></p>
+			<p><label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Menu:', 'better-menu-widget'); ?></label>
+				<select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
+			<?php 
+				foreach ( $menus as $menu ) {
+					$selected = $nav_menu == $menu->term_id ? ' selected="selected"' : '';
+					echo '<option'. $selected .' value="'. $menu->term_id .'">'. $menu->name .'</option>';
+				}
+			?>
+				</select></p>
+			<p><label for="<?php echo $this->get_field_id('menu_class'); ?>"><?php _e('Menu Class:', 'better-menu-widget') ?></label><input type="text" class="widefat" id="<?php echo $this->get_field_id('menu_class'); ?>" name="<?php echo $this->get_field_name('menu_class'); ?>" value="<?php echo $menu_class; ?>" />
+				<small><?php _e( 'CSS class to use for the ul menu element.', 'better-menu-widget' ); ?></small></p>
+				<p class="credits"><small><?php _e('Developed by', 'better-menu-widget'); ?> <a href="http://traceyholinka.com/" rel="nofollow">Tracey Holinka</a></small></p>
+			<?php
+		}
+	}// end class
+	
+} // end if
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== Better Menu Widget ===
+Contributors: traceyholinka
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7TQVKUXW68UHQ
+Tags: widget, menus, navigation
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.2
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Better Menu Widget makes it easy to customize your menu widgets by adding css styles and a heading link.
+
+
+== Description ==
+
+A Custom Menu widget that adds a custom url to the widget title and adds a custom css class. The default custom menu CSS class is `sub-menu` and it is applied to the `<ul>` tag. You will need to style the widget using your theme's stylesheet.
+
+Based on the core WordPress Custom Menu widget.
+
+= Options =
+
+* Custom title URL
+* Custom CSS class
+
+= Plugin's Official Site =
+
+* [Overview](http://www.chaostoclarity.com/wordpress-plugins/better-menu-widget/)
+* [Changelog](http://www.chaostoclarity.com/wordpress-plugins/better-menu-widget/changelog/)
+
+== Installation ==
+
+= Option 1- Automatic Install =
+
+1. Use the built-in plugin installer in your WordPress installation
+2. Search for "Better Menu Widget"
+3. Click "Install Now"
+4. Plugin will automatically install
+
+= Option 2- Manual Install =
+
+1. Download the plugin file to your hard drive
+2. Go to the Plugins management page and Upload 'better-menu-widget.zip' 
+4. Activate the plugin
+
+= Option 3- Manual Install =
+
+1. Download the plugin file to your hard drive
+2. Unzip better-menu-widget.zip
+3. Place the folder 'better-menu-widget' into your plugins directory /wp-content/plugins via ftp
+4. Go to the Plugins management page and Activate the plugin
+
+== Frequently Asked Questions ==
+
+= How do I style a Better Menu Widget? =
+
+You can style a Better Menu Widget by adding the default menu class `sub-menu` to your theme's style sheet. If you've changed the menu class in the widget options, then you would use the new class name you created.
+
+== Screenshots ==
+
+1. Default widget settings
+2. Example widget settings
+3. Front-end view of Better Custom Menu widget
+
+== Changelog ==
+
+= 1.2 =
+
+Internationalize plugin.
+Add developer credits.
+
+= 1.1 =
+
+**Important Change! Read before you update.**
+
+The widget class was changed from "menu-widget" to "better-menu-widget" to avoid class name conflicts. This will cause any widgets you have to disappear so you'll need to add them again. You may also need to update your style sheets.
+
+= 1.0 =
+
+Initial public release.
